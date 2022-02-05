e7c7207c6a372063fd07a132436c097955673ad1
Eva <rutheve.eva@gmail.com>
2022-02-05 18:14:54 +0300

    ft:script add display functionality

--- Js/scripty.js
+++ Js/scripty.js
@@ -1,35 +1,55 @@
+let buttonQuery = document.getElementById("submit");
 
-var buttonQuery = document.getElementById("submit");
-
-buttonQuery.addEventListener("click", function(){
-
-    var score = 0;
-    
+let scoreBoard = document.getElementById("scoreBoard");
 
 
+
+
+function MarkScore(){
+
+  
    var checkingTheAnswer = document.querySelectorAll(".correctAns");
 
 
-
+   score = 0;
    for(var counter = 0; counter < checkingTheAnswer.length; counter++){
        if(checkingTheAnswer[counter].checked == true){
            score+=10;
        }
+   
    }
+   return score;
 
+};
 
+function MessageScore(score){
+    if(score <= 100 && score >= 80){
+        messageToDisplay = "Excellent";
 
-
-   alert("Your Score "+score)
-   
+    }
+    else if(score < 80 && score >= 50){
+        messageToDisplay = "Fair";
+    }
+    else{
+        messageToDisplay = " Poor Score. Please Retake the test"
+    }
     
+    return messageToDisplay;
+
+}
 
 
+function displaying(){
+    scoreBoard.innerHTML = MarkScore() +"</br> "+MessageScore(score);
 
+}
 
+buttonQuery.addEventListener("click",displaying);
 
+close = document.getElementsByClassName("close");
 
-   
+close.addEventListener("click",clearingForm)
 
-}) ;
-
+function clearingForm(){
+    
+}
